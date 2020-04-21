42374c05af8363ed5418af006bb83d47c69414a0
Rachel Warbelow <rachelwarbelow@gmail.com>
2020-04-21 16:24:15 -0600

    Initial commit

---
+++ pp-default-values-alerting.sh
@@ -0,0 +1,31 @@
+!/bin/bash
+
+echo 'Starting test...'
+
+token="&zwnj;"
+
+employers=('walmart' 'fiveguys' 'wm')
+
+for employer in "${employers[@]}"; do 
+    url="https://${employer}.defaultale.guildacceptance.com/partner?auth_redirect=true"
+    if curl -k -l ${url} | cat | grep -q "$token"; then
+        echo "Default content found for ${employer}"
+    else
+        echo "Default content not found"
+    fi
+done
+
+
+
+
+# echo 'Starting test...'
+
+# token="&zwnj;"
+
+# employers=('walmart' 'fiveguys' 'wm')
+
+# for employer in "${employers[@]}"; do
+#   url="https://${employer}.defaultale.guildacceptance.com/partner?auth_redirect=true"
+#   curl -k -o "${employer}-landing.html" ${url}
+# done
+

69b49200c2404add9451cf1f1040e8a11b7e4ffe
Rachel Warbelow <rachelwarbelow@gmail.com>
2020-04-22 17:25:21 -0600

    Changes how URI.open is called

--- default_content_finder.rb
+++ default_content_finder.rb
@@ -7,7 +7,7 @@ pages_with_default_content = Hash.new {|hash, key| hash[key] = [] }
 
 employers.each do |employer|
   url = "https://#{employer}.defaultale.guildacceptance.com/partner?auth_redirect=true"
-  doc = Nokogiri::HTML(open(url, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE))
+  doc = Nokogiri::HTML(URI.open(url, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE))
   doc.traverse do |node|
     if node.text? && node.to_html(encoding:'US-ASCII').include?('&#8204;')
       puts "Found default content for #{employer}: #{node.text}"

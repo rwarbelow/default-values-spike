85c643203c7ee3bca8c911a015ac09aa40abaf2e
Rachel Warbelow <rachelwarbelow@gmail.com>
2020-04-22 17:16:06 -0600

    Updating to Ruby

---
+++ default_content_finder.rb
@@ -0,0 +1,16 @@
+require 'nokogiri'
+require 'open-uri'
+require 'openssl'
+
+employers = ['wm', 'chipotle', 'fiveguys', 'tacobell']
+pages_with_default_content = Hash.new {|hash, key| hash[key] = [] }
+
+employers.each do |employer|
+  url = "https://#{employer}.defaultale.guildacceptance.com/partner?auth_redirect=true"
+  doc = Nokogiri::HTML(open(url, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE))
+  doc.traverse do |node|
+    if node.text? && node.to_html(encoding:'US-ASCII').include?('&#8204;')
+      pages_with_default_content[employer] << node.text
+    end
+  end
+end
--- pp-default-values-alerting.sh
+++ pp-default-values-alerting.sh
@@ -8,17 +8,35 @@ token="&zwnj;"
 echo 'Setting employers'
 employers=('walmart' 'fiveguys' 'wm')
 
+default_content_employers=()
+
+# echo 'Starting loop'
+# for employer in "${employers[@]}"; do 
+#   echo "Checking ${employer}..."
+#   url="https://${employer}.defaultale.guildacceptance.com/partner?auth_redirect=true"
+#   if curl -k -l ${url} | cat | grep -q "$token"; then
+#       echo "Default content found for ${employer}"
+#   else
+#       echo "Default content not found"
+#   fi
+# done
+
+
+
+
 echo 'Starting loop'
 for employer in "${employers[@]}"; do 
   echo "Checking ${employer}..."
   url="https://${employer}.defaultale.guildacceptance.com/partner?auth_redirect=true"
   if curl -k -l ${url} | cat | grep -q "$token"; then
-      echo "Default content found for ${employer}"
+    default_content_employers+=(${employer})
+    echo "Added ${employer} to default_content_employers"
   else
-      echo "Default content not found"
+    echo "Default content not found"
   fi
 done
 
+echo "Partner pages with default content: ${default_content_employers[@]}"
 
 
 

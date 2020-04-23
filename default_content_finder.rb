5c22d044da24636c6db32ce6e0a9304a2b91c949
Rachel Warbelow <rachelwarbelow@gmail.com>
2020-04-22 23:26:23 -0600

    New URL

--- default_content_finder.rb
+++ default_content_finder.rb
@@ -6,8 +6,8 @@ employers = ['wm', 'chipotle', 'fiveguys', 'tacobell']
 pages_with_default_content = Hash.new {|hash, key| hash[key] = [] }
 
 employers.each do |employer|
-  url = "https://#{employer}.guildacceptance.com/partner?auth_redirect=true"
-  doc = Nokogiri::HTML(URI.open(url, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE, allow_redirections: :all))
+  url = "https://#{employer}.previewapi.guildacceptance.com/partner?auth_redirect=true"
+  doc = Nokogiri::HTML(URI.open(url, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE))
   doc.traverse do |node|
     if node.text? && node.to_html(encoding:'US-ASCII').include?('&#8204;')
       pages_with_default_content[employer] << node.to_html(encoding:'US-ASCII').gsub('&#8204;', '')

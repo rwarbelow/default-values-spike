262cca06fab50dfc1a47a223429e045b3c3f01cd
Rachel Warbelow <rachelwarbelow@gmail.com>
2020-04-23 17:12:58 -0600

    Switches &zwnj; for three spaces

--- default_content_finder.rb
+++ default_content_finder.rb
@@ -9,14 +9,14 @@ employers.each do |employer|
   url = "https://#{employer}.defaultale.guildacceptance.com/partner?auth_redirect=true"
   doc = Nokogiri::HTML(URI.open(url, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE))
   doc.traverse do |node|
-    if node.text? && node.to_html(encoding:'US-ASCII').include?('&#8204;')
-      pages_with_default_content[employer] << node.to_html(encoding:'US-ASCII').gsub('&#8204;', '')
+    if node.text? && node.text.include?('   ')
+      pages_with_default_content[employer] << node.text.gsub('   ', '')
     end
   end
 end
 
 error_results_message = pages_with_default_content.map do |employer, content|
-  ":alert: *#{employer.upcase}* is using the following default content: #{content.map(&:strip).map.with_index {|content, i| "\n\t_" + (i+1).to_s + ". " + content + "_"}.join}"
+  ":alert: *#{employer.upcase}'s* partner page is displaying the following pieces of default content: #{content.map(&:strip).map.with_index {|content, i| "\n\t_" + (i+1).to_s + ". " + content + "_"}.join}"
 end.join("\n\n")
 
 if pages_with_default_content.count > 0
